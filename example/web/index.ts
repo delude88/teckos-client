cb3f85411189e09f99f50df44f3c0377b9eeff4e
delude88 <tobias.hegemann@googlemail.com>
2022-02-23 13:24:05 +0100

    Updated packages

--- example/web/index.ts
+++ example/web/index.ts
@@ -6,82 +6,95 @@ const sendingRef = document.getElementById("sending") as HTMLTextAreaElement;
 const receivingRef = document.getElementById("receiving") as HTMLTextAreaElement;
 
 const printToSent = (text: string) => {
-  sendingRef.value += text + "\n";
+    sendingRef.value += text + "\n";
 }
 const printToReceive = (text: string) => {
-  receivingRef.value += text + "\n";
+    receivingRef.value += text + "\n";
 }
 
 const sendExampleMessages = (ws: ITeckosClient) => {
-  printToSent("Sending 'no-args'");
-  ws.emit('no-args');
+    printToSent("Sending 'undefined'");
+    ws.emit(undefined);
 
-  printToSent("Sending 'hello' with payload");
-  ws.emit('hello', {another: 'world'}, ['hello', 'world']);
+    printToSent('sending undefined');
+    ws.emit(undefined);
 
-  printToSent("Sending 'hello' with payload and callback");
-  ws.emit('work', {some: 'data'}, (result: string) => {
-    printToReceive("Got result from callback: " + result);
-  });
+    printToSent("Sending larger payload'");
+    ws.emit("send-ice-candidate", {
+        from: "6216171fbc99489419eadc7e",
+        to: "62161b6ebc99489419eadc88",
+        iceCandidate: undefined
+    });
 
-  printToSent("Sending 'personal'");
-  ws.emit('personal');
+    printToSent("Sending 'no-args'");
+    ws.emit('no-args');
 
-  printToSent("Now sleeping for 5s...");
+    printToSent("Sending 'hello' with payload");
+    ws.emit('hello', {another: 'world'}, ['hello', 'world']);
 
-  setTimeout(() => sendExampleMessages(ws), 5000);
+    printToSent("Sending 'hello' with payload and callback");
+    ws.emit('work', {some: 'data'}, (result: string) => {
+        printToReceive("Got result from callback: " + result);
+    });
+
+    printToSent("Sending 'personal'");
+    ws.emit('personal');
+
+    printToSent("Now sleeping for 5s...");
+
+    setTimeout(() => sendExampleMessages(ws), 5000);
 }
 
 const USE_TOKEN = true
 
 const connect = () => {
-  const ws = USE_TOKEN ? new TeckosClientWithJWT(URL, {
-    reconnection: true,
-    debug: true
-  }, "mytoken") : new TeckosClient(URL, {reconnection: true, debug: true})
+    const ws = USE_TOKEN ? new TeckosClientWithJWT(URL, {
+        reconnection: true,
+        debug: true
+    }, "mytoken") : new TeckosClient(URL, {reconnection: true, debug: true})
 
-  ws.on('connect', () => {
-    printToReceive("Connected!");
-    sendExampleMessages(ws);
-  });
+    ws.on('connect', () => {
+        printToReceive("Connected!");
+        sendExampleMessages(ws);
+    });
 
-  ws.on('hello', () => {
-    printToReceive("Received 'hello' (was broadcasted by server to all)");
-  })
+    ws.on('hello', () => {
+        printToReceive("Received 'hello' (was broadcasted by server to all)");
+    })
 
-  ws.on('notification', () => {
-    printToReceive("Received 'notification' (was broadcasted by server to group 'usergroup')");
-  })
+    ws.on('notification', () => {
+        printToReceive("Received 'notification' (was broadcasted by server to group 'usergroup')");
+    })
 
-  ws.on('test', () => {
-    printToReceive("Received 'test'");
-  })
+    ws.on('test', () => {
+        printToReceive("Received 'test'");
+    })
 
-  ws.on('error', (error: Error) => {
-    printToReceive("Received 'error': " + error);
-  });
+    ws.on('error', (error: Error) => {
+        printToReceive("Received 'error': " + error);
+    });
 
-  ws.on('disconnect', () => {
-    printToReceive("Disconnected!");
-  });
+    ws.on('disconnect', () => {
+        printToReceive("Disconnected!");
+    });
 
-  ws.on('reconnect_attempt', () => {
-    printToReceive("reconnect_attempt!");
-  });
+    ws.on('reconnect_attempt', () => {
+        printToReceive("reconnect_attempt!");
+    });
 
-  ws.on('reconnect', () => {
-    printToReceive("Reconnected!");
-  });
+    ws.on('reconnect', () => {
+        printToReceive("Reconnected!");
+    });
 
-  ws.on('disconnect', () => {
-    printToReceive("Another disconnection handler!");
-  });
+    ws.on('disconnect', () => {
+        printToReceive("Another disconnection handler!");
+    });
 
-  console.log("Connecting...");
-  ws.connect();
+    console.log("Connecting...");
+    ws.connect();
 };
 
 
 document.addEventListener("DOMContentLoaded", function () {
-  connect();
+    connect();
 });
--- src/util/Converter.ts
+++ src/util/Converter.ts
@@ -1,31 +1,10 @@
-import * as Bowser from 'bowser'
 import { Packet } from '../types'
 
 const enc = new TextEncoder()
 const dec = new TextDecoder()
 
-const isWebkit15 =
-    (window !== undefined &&
-        Bowser.getParser(window.navigator.userAgent).satisfies({
-            safari: '~15',
-        })) ||
-    false
-
-const encodePacketDefault = (packet: Packet): ArrayBufferLike => enc.encode(JSON.stringify(packet))
-
-const encodePacketOnWebkit15 = (packet: Packet): ArrayBufferLike => {
-    let encoded = JSON.stringify(packet)
-    if (encoded.length < 25) {
-        encoded = JSON.stringify({
-            ...packet,
-            data: [packet.data, 'additionalpayload'],
-        })
-    }
-    return enc.encode(encoded)
-}
-
-const encodePacket = isWebkit15 ? encodePacketDefault : encodePacketOnWebkit15
-
+const encodePacket = (packet: Packet): ArrayBufferLike => enc.encode(JSON.stringify(packet))
 const decodePacket = (buffer: ArrayBuffer): Packet =>
     JSON.parse(dec.decode(buffer).toString()) as Packet
+
 export { encodePacket, decodePacket }
