c37a84765e9a84e1d0769bf1aaeda7594c903c93
Tobias Hegemann <tobias.hegemann@googlemail.com>
2022-06-24 00:12:47 +0200

    v0.3.4

--- src/types/ConnectionState.js
+++
@@ -1,9 +0,0 @@
-var ConnectionState;
-(function (ConnectionState) {
-    ConnectionState["DISCONNECTED"] = "disconnected";
-    ConnectionState["CONNECTING"] = "connecting";
-    ConnectionState["CONNECTED"] = "connected";
-    ConnectionState["DISCONNECTING"] = "disconnecting";
-})(ConnectionState || (ConnectionState = {}));
-export { ConnectionState };
-//# sourceMappingURL=ConnectionState.js.map
